b80259319134cbb0f7139644b503c1ac438d950f
Victor <victor@MacBook-Pro-Victor.local>
2023-08-30 22:24:49 +0200

    add libs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
-  root: true,
-  extends: '@react-native',
+    parser: '@babel/eslint-parser',
+    plugins: ['react', 'react-native'],
+    env: {
+        'react-native/react-native': true,
+    },
+    extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:react-native/all'],
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,6 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
   tabWidth: 4,
   printWidth: 120,
+  singleQuote: true,
+  endOfLine: 'lf',
 };
